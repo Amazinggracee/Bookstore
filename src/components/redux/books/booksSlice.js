4cf3076b7d63ce8fcba6aba17300877840605a7c
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-05-08 07:58:46 +0100

    Add the redux contents

---
+++ src/components/redux/books/booksSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const booksSlice = createSlice({
+  name: 'books',
+  initialState: { books: [] },
+  reducers: {
+    addBook: (state, action) => {
+      state.books.push(action.payload);
+    },
+    removeBook: (state, action) => {
+      state.books = state.books.filter((book) => book.id !== action.payload);
+    },
+  },
+});
+
+export const { addBook, removeBook } = booksSlice.actions;
+
+export default booksSlice.reducer;
