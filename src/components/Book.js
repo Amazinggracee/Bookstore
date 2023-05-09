a3b2f951538fa7615b61625e5259e98dbef93593
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-05-09 21:00:28 +0100

    Add Redux useDispatch Hook

---
+++ src/components/Book.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
+
+export default function Book({
+  id, title, author, category,
+}) {
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div className="booklist">
+        <h2>List of Books</h2>
+        <ul>
+          <li className="bookitems">
+            <p>{title}</p>
+            <p>{author}</p>
+            <p>{category}</p>
+            <button
+              type="button"
+              className="button key"
+              onClick={() => dispatch(removeBook(id))}
+            >
+              Remove
+            </button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
+Book.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+};
