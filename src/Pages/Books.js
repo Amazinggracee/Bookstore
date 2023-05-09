b651eb5dbed5abffefbf16c97eb23ea74725339f
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-05-09 21:01:59 +0100

    Add Pages component with books.js and categories.js functions

---
+++ src/Pages/Books.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Book from '../components/Book';
+import Form from '../components/Form';
+
+function Books() {
+  const { bookList } = useSelector((state) => state.books);
+  return (
+    <>
+      <div>
+        {bookList.map((book) => (
+          <Book
+            key={book.item_id}
+            id={book.item_id}
+            title={book.title}
+            author={book.author}
+            category={book.category}
+          />
+        ))}
+        <Form />
+      </div>
+    </>
+  );
+}
+export default Books;
---
+++ src/Pages/Categories.js
@@ -0,0 +1,11 @@
+function Categories() {
+  return (
+    <div>
+      <button type="button" className="button key">
+        Check status
+      </button>
+    </div>
+  );
+}
+
+export default Categories;
